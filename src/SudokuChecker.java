ec048abe303b57e14dc2888978ca4f1457184612
subudhiroshan <subudhiroshan@gmail.com>
2013-09-19 11:41:25 -0400

    Added User input

--- src/SudokuChecker.java
+++ src/SudokuChecker.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Author: Roshan C Subudhi
  * USC, Columbia
@@ -108,100 +112,123 @@ public class SudokuChecker {
 		
 		int[][] board = new int[9][9];
 		
-		board[0][0] = 5;
-		board[0][1] = 3;
-		board[0][2] = 4;
-		board[0][3] = 6;
-		board[0][4] = 7;
-		board[0][5] = 8;
-		board[0][6] = 9;
-		board[0][7] = 1;
-		board[0][8] = 2;
+//		board[0][0] = 5;
+//		board[0][1] = 3;
+//		board[0][2] = 4;
+//		board[0][3] = 6;
+//		board[0][4] = 7;
+//		board[0][5] = 8;
+//		board[0][6] = 9;
+//		board[0][7] = 1;
+//		board[0][8] = 2;
+//		
+//		board[1][0] = 6;
+//		board[1][1] = 7;
+//		board[1][2] = 2;
+//		board[1][3] = 1;
+//		board[1][4] = 9;
+//		board[1][5] = 5;
+//		board[1][6] = 3;
+//		board[1][7] = 4;
+//		board[1][8] = 8;
+//		
+//		board[2][0] = 1;
+//		board[2][1] = 9;
+//		board[2][2] = 8;
+//		board[2][3] = 3;
+//		board[2][4] = 4;
+//		board[2][5] = 2;
+//		board[2][6] = 5;
+//		board[2][7] = 6;
+//		board[2][8] = 7;
+//		
+//		board[3][0] = 8;
+//		board[3][1] = 5;
+//		board[3][2] = 9;
+//		board[3][3] = 7;
+//		board[3][4] = 6;
+//		board[3][5] = 1;
+//		board[3][6] = 4;
+//		board[3][7] = 2;
+//		board[3][8] = 3;
+//		
+//		board[4][0] = 4;
+//		board[4][1] = 2;
+//		board[4][2] = 6;
+//		board[4][3] = 8;
+//		board[4][4] = 5;
+//		board[4][5] = 3;
+//		board[4][6] = 7;
+//		board[4][7] = 9;
+//		board[4][8] = 1;
+//		
+//		board[5][0] = 7;
+//		board[5][1] = 1;
+//		board[5][2] = 3;
+//		board[5][3] = 9;
+//		board[5][4] = 2;
+//		board[5][5] = 4;
+//		board[5][6] = 8;
+//		board[5][7] = 5;
+//		board[5][8] = 6;
+//		
+//		board[6][0] = 9;
+//		board[6][1] = 6;
+//		board[6][2] = 1;
+//		board[6][3] = 5;
+//		board[6][4] = 3;
+//		board[6][5] = 7;
+//		board[6][6] = 2;
+//		board[6][7] = 8;
+//		board[6][8] = 4;
+//		
+//		board[7][0] = 2;
+//		board[7][1] = 8;
+//		board[7][2] = 7;
+//		board[7][3] = 4;
+//		board[7][4] = 1;
+//		board[7][5] = 9;
+//		board[7][6] = 6;
+//		board[7][7] = 3;
+//		board[7][8] = 5;
+//		
+//		board[8][0] = 3;
+//		board[8][1] = 4;
+//		board[8][2] = 5;
+//		board[8][3] = 2;
+//		board[8][4] = 8;
+//		board[8][5] = 6;
+//		board[8][6] = 1;
+//		board[8][7] = 7;
+//		board[8][8] = 9;
 		
-		board[1][0] = 6;
-		board[1][1] = 7;
-		board[1][2] = 2;
-		board[1][3] = 1;
-		board[1][4] = 9;
-		board[1][5] = 5;
-		board[1][6] = 3;
-		board[1][7] = 4;
-		board[1][8] = 8;
-		
-		board[2][0] = 1;
-		board[2][1] = 9;
-		board[2][2] = 8;
-		board[2][3] = 3;
-		board[2][4] = 4;
-		board[2][5] = 2;
-		board[2][6] = 5;
-		board[2][7] = 6;
-		board[2][8] = 7;
-		
-		board[3][0] = 8;
-		board[3][1] = 5;
-		board[3][2] = 9;
-		board[3][3] = 7;
-		board[3][4] = 6;
-		board[3][5] = 1;
-		board[3][6] = 4;
-		board[3][7] = 2;
-		board[3][8] = 3;
-		
-		board[4][0] = 4;
-		board[4][1] = 2;
-		board[4][2] = 6;
-		board[4][3] = 8;
-		board[4][4] = 5;
-		board[4][5] = 3;
-		board[4][6] = 7;
-		board[4][7] = 9;
-		board[4][8] = 1;
-		
-		board[5][0] = 7;
-		board[5][1] = 1;
-		board[5][2] = 3;
-		board[5][3] = 9;
-		board[5][4] = 2;
-		board[5][5] = 4;
-		board[5][6] = 8;
-		board[5][7] = 5;
-		board[5][8] = 6;
-		
-		board[6][0] = 9;
-		board[6][1] = 6;
-		board[6][2] = 1;
-		board[6][3] = 5;
-		board[6][4] = 3;
-		board[6][5] = 7;
-		board[6][6] = 2;
-		board[6][7] = 8;
-		board[6][8] = 4;
-		
-		board[7][0] = 2;
-		board[7][1] = 8;
-		board[7][2] = 7;
-		board[7][3] = 4;
-		board[7][4] = 1;
-		board[7][5] = 9;
-		board[7][6] = 6;
-		board[7][7] = 3;
-		board[7][8] = 5;
-		
-		board[8][0] = 3;
-		board[8][1] = 4;
-		board[8][2] = 5;
-		board[8][3] = 2;
-		board[8][4] = 8;
-		board[8][5] = 6;
-		board[8][6] = 1;
-		board[8][7] = 7;
-		board[8][8] = 9;
+		System.out.println("Please enter the solution row by row:");
+		for (int i=0; i<9; i++){
+			System.out.println("Row " + (i+1) + "- ");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String row = null;
+			try {
+				row = br.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (row.length() == 9){
+				for (int j=0; j<9; j++){
+					board[i][j] = Integer.parseInt(row.substring(j, j+1));
+				}
+			}else{
+				System.out.println("Wrong input. Try again!");
+				i = i-1;
+				continue;
+			}
+		}
 
 		SudokuChecker sc = new SudokuChecker(board);
+		System.out.println();
 		System.out.println("The submitted grid is: ");
 		sc.drawGrid();
-		
+		System.out.println();
 		if (sc.rows() && sc.columns() && sc.boxes()){
 			System.out.println("This is a valid Sudoku Solution.");
 		}else{
